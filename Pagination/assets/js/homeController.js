7f003e63af1a1070823b1a77afc894f0eb8efb6c
Jean Carlos de Almeida <jeanjnx@gmail.com>
2015-12-01 16:47:25 -0200

    Initial implementation

---
+++ Pagination/assets/js/homeController.js
@@ -0,0 +1,40 @@
+﻿angular.module('app.controllers', [])
+  .controller('HomeController', function ($scope) {
+      $scope.pageSize = 10;
+      
+
+      $scope.get = function (page) {
+          debugger;
+          var list = getTestList();
+          list = skip(list, page * $scope.pageSize);
+          list = take(list, $scope.pageSize);
+          $scope.list = list;
+      }
+
+      function getTestList() {
+          var list = [];
+          for (var i = 0; i < 100; i++) {
+              var item = { id: i, name: 'test_' + i };
+              list.push(item);
+          }
+          return list;
+      }
+
+      function skip(list, value) {
+          var newList = [];
+          for (var i = value; i < list.length; i++) {
+              newList.push(list[i]);
+          }
+          return newList;
+      }
+
+      function take(list, value) {
+          var newList = [];
+          for (var i = 0; i < value; i++) {
+              newList.push(list[i]);
+          }
+          return newList;
+      }
+
+      $scope.get(0);
+  });
---
+++ Pagination/assets/js/pagination.js
@@ -0,0 +1,29 @@
+﻿var directives = angular.module('app.directives', []);
+directives.directive("pagination", function ($http) {
+    return function ($scope, elm, attrs) {
+        debugger;
+        var list = $scope[attrs.list]
+        $scope.pages = getPages(list);
+
+        $scope.goTo = function (page) {
+            debugger;
+            list = $scope[attrs.get](page);
+            $scope.pages = getPages(list);
+        };
+
+        function getPages(list) {
+            var pages = [];
+
+            for (var i = 0; i < list.length; i++) {
+                pages.push(i);
+            }
+
+            return pages;
+        }
+    }
+})
+.directive("paginationHtml", function () {
+    return {
+        templateUrl: 'assets/tpl/pagination.html'
+    }
+});
